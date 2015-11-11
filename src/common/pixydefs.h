95138017443d662433afefb3e1b34cc80a665ab3
ThomasSFL <tstone7@gatech.edu>
2015-11-11 17:51:58 -0500

    first commit
    first commit

---
+++ src/common/pixydefs.h
@@ -0,0 +1,24 @@
+//
+// begin license header
+//
+// This file is part of Pixy CMUcam5 or "Pixy" for short
+//
+// All Pixy source code is provided under the terms of the
+// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
+// Those wishing to use Pixy source code, software and/or
+// technologies under different licensing terms should contact us at
+// cmucam@cs.cmu.edu. Such licensing terms are available for
+// all portions of the Pixy codebase presented here.
+//
+// end license header
+//
+
+#ifndef __PIXYDEFS_H__
+#define __PIXYDEFS_H__
+
+#define PIXY_VID       0xB1AC
+#define PIXY_DID       0xF000
+#define PIXY_DFU_VID   0x1FC9
+#define PIXY_DFU_DID   0x000C
+
+#endif
